a796919f910d97d5adc6becd8a0188aafa849d44
Zhexin Wu <zhexwu@eu-login-18.euler.ethz.ch>
2023-04-21 16:20:17 +0200

    added BO

--- scripts/run_test_retest.py
+++ scripts/run_test_retest.py
@@ -12,6 +12,7 @@ import numpy as np
 import pandas as pd
 import glob
 import pickle
+import warnings
 
 from bayes_opt import BayesianOptimization
 from bayes_opt.logger import JSONLogger
@@ -59,17 +60,18 @@ def process_one_subject(dir_name: str, config_dict: dict, args_dict: dict):
     retest_centroids = model_retest.model.cluster_centers_
 
     # save results
-    subject_idx = os.path.basename(dir_name)  # subject{num}
-    output_dir = os.path.join(args_dict["output_dir"], subject_idx)
-    if not os.path.isdir(output_dir):
-        os.makedirs(output_dir)
+    if not args_dict["if_tuning"]:
+        subject_idx = os.path.basename(dir_name)  # subject{num}
+        output_dir = os.path.join(args_dict["output_dir"], subject_idx)
+        if not os.path.isdir(output_dir):
+            os.makedirs(output_dir)
 
-    save_image(test_labels, os.path.join(output_dir, "test_atlas.nii.gz"))
-    save_image(retest_labels, os.path.join(output_dir, "retest_atlas.nii.gz"))
-    with open(os.path.join(output_dir, "test_centroids.pkl"), "wb") as wf:
-        pickle.dump(test_centroids, wf)
-    with open(os.path.join(output_dir, "retest_centroids.pkl"), "wb") as wf:
-        pickle.dump(retest_centroids, wf)
+        save_image(test_labels, os.path.join(output_dir, "test_atlas.nii.gz"))
+        save_image(retest_labels, os.path.join(output_dir, "retest_atlas.nii.gz"))
+        with open(os.path.join(output_dir, "test_centroids.pkl"), "wb") as wf:
+            pickle.dump(test_centroids, wf)
+        with open(os.path.join(output_dir, "retest_centroids.pkl"), "wb") as wf:
+            pickle.dump(retest_centroids, wf)
     
     return test_labels, retest_labels, test_centroids, retest_centroids
 
@@ -81,6 +83,12 @@ if __name__ == "__main__":
     parser.add_argument("--output_dir", default="../outputs_clustering/test_retest")  # save logging here
     parser.add_argument("--if_tuning", action="store_true")
     parser.add_argument("--eval_mode", choices=["dsc", "hausdorff_dist", "centroid_dist"], default="dsc")
+    parser.add_argument("--spherical_scale_log_min", type=float, default=0)
+    parser.add_argument("--spherical_scale_log_max", type=float, default=3)
+    parser.add_argument("--seed", type=int, default=0)
+    parser.add_argument("--if_print_in_objective_func", action="store_true")
+    parser.add_argument("--init_points", type=int, default=5)
+    parser.add_argument("--num_iters", type=int, default=30)
 
     args_dict = vars(parser.parse_args())
     config_dict = load_config(args_dict["task_name"])
@@ -89,10 +97,19 @@ if __name__ == "__main__":
         "num_clusters": 7,
         "centroid_dist_order": 2
     }
+    with open(os.path.join(args_dict["output_dir"], "desc.txt"), "w") as wf:
+        for key, val in args_dict.items():
+            wf.write(f"{key}: {val}\n")
+        wf.write("-" * 100)
+        wf.write("\n")
+        for key, val in config_dict.items():
+            wf.write(f"{key}: {val}\n")
 
-    def objective_func(spherical_scale: float, spatial_weight: float):
+    warnings.filterwarnings("ignore")
+    def objective_func(spherical_scale_log: float, spatial_weight: float):
         config_dict_cp = config_dict.copy()
         if args_dict["if_tuning"]:
+            spherical_scale = 10 ** spherical_scale_log
             config_dict_cp["features"].update({
                 "spherical_scale": spherical_scale,
                 "spatial_weight": spatial_weight
@@ -101,7 +118,10 @@ if __name__ == "__main__":
         retest_labels_all = []
         test_centroids_all = []
         retest_centroids_all = []
-        for dir_name in glob.glob(os.path.join(args_dict["input_dir"], "*")):
+        all_dirs = glob.glob(os.path.join(args_dict["input_dir"], "*"))
+        for i, dir_name in enumerate(all_dirs):
+            if args_dict["if_print_in_objective_func"]:
+                print(f"Current: subject {i + 1}/{len(all_dirs)}")
             test_labels, retest_labels, test_centroids, retest_centroids = process_one_subject(dir_name, config_dict, args_dict)
             test_labels_all.append(test_labels)
             retest_labels_all.append(retest_labels)
@@ -113,9 +133,26 @@ if __name__ == "__main__":
 
         return metric_df[args_dict["eval_mode"]].mean()
 
+    best_spherical_scale_log = np.log10(config_dict["features"]["spherical_scale"])
+    best_spatial_weight = config_dict["features"]["spatial_weight"]
     if args_dict["if_tuning"]:
-        # TODO
-        pass
+        pbounds = {
+            "spherical_scale_log": (args_dict["spherical_scale_log_min"], args_dict["spherical_scale_log_max"]),
+            "spatial_weight": (0, 1)
+        }
+        opt = BayesianOptimization(objective_func, pbounds, random_state=args_dict["seed"], verbose=2)
+        logger = JSONLogger(path=os.path.join(args_dict["output_dir"], "logs.json"))
+        opt.subscribe(Events.OPTIMIZATION_STEP, logger)
+        
+        print("Performing hyperparam search...")
+        opt.maximize(init_points=args_dict["init_points"], n_iter=args_dict["num_iters"])
+        best_spherical_scale_log = opt.max["params"]["spherical_scale_log"]
+        best_spatial_weight = opt.max["params"]["spatial_weight"]
+        args_dict["if_tuning"] = False  # required, since args_dict is used in objective_func(.)
     
-    else:
-         objective_func(0., 0.)  # set at random since config_dict is used
+    with open(os.path.join(args_dict["output_dir"], "desc.txt"), "a") as wf:
+        wf.write("-" * 100)
+        wf.write("\n")
+        wf.write(f"best_spherical_scale: {10 ** best_spherical_scale_log}, best_spatial_weight: {best_spatial_weight}\n")
+    args_dict["if_print_in_objective_func"] = True
+    objective_func(best_spherical_scale_log, best_spatial_weight)
